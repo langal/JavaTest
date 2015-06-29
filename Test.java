d4a595cfaa20b5391894c158ab8b9361f446c12f
Alex Lang <alex.lang@gmail.com>
2015-06-29 13:57:54 -0700

    second

--- Test.java
+++ Test.java
@@ -1,30 +1,48 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.text.SimpleDateFormat;
 import java.lang.Iterable;
 
 public class Test {
 
     /**
+     * A node of a tree
      */
     public class Node {
         private NodeAble data;
-        private LinkedList<Node> list;
+        private ArrayList<NodeAble> list;
 
         public Node(NodeAble object) {
             this.data = object;
-            this.list = new LinkedList<Node>();
+            this.list = new ArrayList<NodeAble>();
         }
 
+        /**
+         * 
+         */
         public Node traverseDepth() throws Exception {
-            System.out.println(this.data.getOutput());
+            this.list.add(data);
             if (this.data.list() == null) {
                 return this;
             }
             for (NodeAble subNode : this.data.list()) {
                 Node child = new Node(subNode);
-                child.traverseDepth();        
+                this.list.addAll(child.traverseDepth().list);
+            }
+            return this;
+        }
+
+        /**
+         *
+         */
+        public Node traverseBreadth() throws Exception {
+            this.list.add(data);
+            if (this.data.list() == null) {
+                return this;
+            }
+            for (NodeAble subNode : this.data.list()) {
+                Node child = new Node(subNode);
+                this.list.addAll(child.traverseDepth().list);
             }
             return this;
         }
@@ -36,6 +54,7 @@ public class Test {
     public interface NodeAble {
         public Iterable<NodeAble> list();
         public String getOutput();
+        public String getSimpleOutput();
     }
 
     /**
@@ -79,14 +98,39 @@ public class Test {
             output = output + "\t" + file.length();
             return output;
         }
+
+        /**
+         * Just return a String with file path
+         * @return String with the file path
+         */
+        public String getSimpleOutput() {
+            return file.getAbsolutePath();
+        }
     }
 
     public static void main(String argc[]) {
         try {
             Test test = new Test();
             FileNodeAble root = test.new FileNodeAble(new File(argc[0]));
-            Test.Node rootNode = test.new Node(root);
-            rootNode.traverseDepth();
+            if (argc.length > 1 && argc[1].equalsIgnoreCase("breadth")) {
+                Test.Node rootNode = test.new Node(root);
+                rootNode.traverseBreadth();
+                for (NodeAble nodeAble : rootNode.list) {
+                    System.out.println(nodeAble.getSimpleOutput());
+                }
+                for (NodeAble nodeAble : rootNode.list) {
+                    System.out.println(nodeAble.getOutput());
+                }
+            } else {
+                Test.Node rootNode = test.new Node(root);
+                rootNode.traverseDepth();
+                for (NodeAble nodeAble : rootNode.list) {
+                    System.out.println(nodeAble.getSimpleOutput());
+                }
+                for (NodeAble nodeAble : rootNode.list) {
+                    System.out.println(nodeAble.getOutput());
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
